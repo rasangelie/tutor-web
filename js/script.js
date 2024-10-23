7dbccc255142a92733147f693580b090d8e23d0b
angelie <angeliebadar1@gmail.com>
2024-10-23 17:31:46 +0800

    feat: add bg on scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Toggle Mobile Menu
 document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.querySelector(".mobile-menu-toggle");
   const mobileMenu = document.querySelector(".mobile-menu-items");
@@ -5,4 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
   toggleButton.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
   });
+});
+
+//Change navbar bg on scroll
+const nav = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    nav.classList.add("navbar-scroll");
+  } else {
+    nav.classList.remove("navbar-scroll");
+  }
 });
